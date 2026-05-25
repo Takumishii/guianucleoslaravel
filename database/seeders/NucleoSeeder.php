fd4d81dfcb67ce3b2b4c27c9ce3aebe756731ec6
Geraldine Martínez <gemarcid@gmail.com>
2026-05-24 21:32:43 -0400

    Update NucleoSeeder.php

--- database/seeders/NucleoSeeder.php
+++ database/seeders/NucleoSeeder.php
@@ -2,16 +2,38 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Nucleo;
 use Illuminate\Database\Seeder;
 
 class NucleoSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $nucleos = [
+            [
+                'nombre' => 'NT1 - Introducción a Laravel',
+                'descripcion' => 'Fundamentos del framework Laravel, arquitectura MVC, Composer, Artisan y estructura del proyecto.',
+            ],
+            [
+                'nombre' => 'NT2 - Rutas y Controladores',
+                'descripcion' => 'Uso de rutas HTTP, parámetros, controladores, métodos RESTful y middleware.',
+            ],
+            [
+                'nombre' => 'NT3 - Blade Templates',
+                'descripcion' => 'Uso de vistas Blade, directivas, layouts, componentes y paso de variables.',
+            ],
+            [
+                'nombre' => 'NT4 - Modelos y Base de Datos',
+                'descripcion' => 'Migraciones, modelos Eloquent, relaciones, seeders, factories y consultas.',
+            ],
+            [
+                'nombre' => 'NT5 - Formularios y Validaciones',
+                'descripcion' => 'Formularios HTML en Blade, CSRF, validaciones, Form Request y mensajes de error.',
+            ],
+        ];
+
+        foreach ($nucleos as $nucleo) {
+            Nucleo::create($nucleo);
+        }
     }
 }
