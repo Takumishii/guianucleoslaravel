28fc120513ee28d5fd1f65eb7db7a7ee208d4bb8
Geraldine Martínez <gemarcid@gmail.com>
2026-05-24 23:36:16 -0400

    Update Nucleo.php

--- app/Models/Nucleo.php
+++ app/Models/Nucleo.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Nucleo extends Model
 {
-    //
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+    ];
+
+    public function apuntes(): HasMany
+    {
+        return $this->hasMany(Apunte::class);
+    }
 }
